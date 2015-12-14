119e577122aa5aa2d4e03d0b43c4d45473d81b80
liuxiangtao <liuxiangtao@baidu.com>
2015-12-14 21:32:38 +0800

    Initial Commit

---
+++ BWPlayer/BWPlayer.h
@@ -0,0 +1,13 @@
+//
+//  BWPlayer.h
+//  BWPlayer
+//
+//  Created by liuxiangtao on 15/12/14.
+//  Copyright © 2015年 liuxiangtao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BWPlayer : NSObject
+
+@end
---
+++ BWPlayer/BWPlayer.m
@@ -0,0 +1,13 @@
+//
+//  BWPlayer.m
+//  BWPlayer
+//
+//  Created by liuxiangtao on 15/12/14.
+//  Copyright © 2015年 liuxiangtao. All rights reserved.
+//
+
+#import "BWPlayer.h"
+
+@implementation BWPlayer
+
+@end
